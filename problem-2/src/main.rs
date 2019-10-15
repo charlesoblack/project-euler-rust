9962a724c5fae2179fa2759db79e9959d7fd84e6
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2019-10-15 19:22:56 -0400

    problem 2 solved

--- problem-2/src/main.rs
+++ problem-2/src/main.rs
@@ -1,10 +1,19 @@
 fn main() {
     println!("Hello, world!");
 
-    let num_2: i8 = 1;
-    let num_1: i8 = 1;
+    let mut num_2: i32;
+    let mut num_1: i32 = 1;
+    let mut num_0: i32 = 1;
+    let mut sum: i32 = 0;
     let limit: i32 = 4 * 10_i32.pow(6);
 
-    println!("{}", limit);
-
+    while num_0 < limit {
+        if num_0 % 2 == 0 {
+            sum += num_0;
+        }
+        num_2 = num_1;
+        num_1 = num_0;
+        num_0 += num_2;
+    }
+    println!("{}", sum);
 }
